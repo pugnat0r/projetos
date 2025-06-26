a7f8e35e5202623c9441f827e9ba2a05137926c1
pugnator <pugnator_winchester@hotmail.com>
2025-06-26 01:03:16 -0300

    feat: Add operadores_associação.py

---
+++ 02_Python_Santander_Bootcamp/operadores_associação.py
@@ -0,0 +1,12 @@
+curso = "Curso de Python"
+frutas = "laranja", "uva" "limão"
+saques = [1500, 100]
+
+"Python" in curso
+# >>> True
+
+"maçã" not in frutas
+# >>> True
+
+200 in saques
+# >>> False
