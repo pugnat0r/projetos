0e5343b22eedb8b982f10507c85f1300728f1308
pugnator <pugnator_winchester@hotmail.com>
2025-09-26 22:38:37 -0300

    Feat: Edit V3

--- 00_python/01_Projetos/Sistema_Báncario/Projeto_Sistema_Báncario_v3.py
+++ 00_python/01_Projetos/Sistema_Báncario/Projeto_Sistema_Báncario_v3.py
@@ -7,6 +7,7 @@ import customtkinter
 class WalletApp(customtkinter.CTk):
 
     def __init__(self):
+
         super().__init__()
         self.geometry("600x500")
         self.title("Pugnator Wallet")
@@ -39,9 +40,9 @@ class WalletApp(customtkinter.CTk):
 
 
 class WalletLogin(customtkinter.CTkFrame):
-
-    def __init__(self):
-        super().__init__()
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
@@ -66,7 +67,6 @@ class WalletLogin(customtkinter.CTkFrame):
         )
         self.label1.grid(row=1, column=1, pady=1)
 
-        # Campo de entrada para o nome de usuário
         self.entry_username = customtkinter.CTkEntry(
             self,
             placeholder_text="     CPF do usuário"
@@ -75,30 +75,141 @@ class WalletLogin(customtkinter.CTkFrame):
 
         self.entry_password = customtkinter.CTkEntry(
             self,
-            placeholder_text="     Senha do usuário"
+            placeholder_text="     Senha do usuário",
+            show="*"  # opcional para esconder senha
         )
         self.entry_password.grid(row=1, column=1, pady=(160, 5))
 
         self.button0 = customtkinter.CTkButton(
             self,
             text="Entrar",
-            command=self.destroy
-        )
+            command=lambda: controller.show_frame(HomePage))
         self.button0.grid(row=1, column=1, pady=(240, 5))
 
         self.button1 = customtkinter.CTkButton(
             self,
             text="Cadastrar-se",
-            command=self.destroy
-        )
+            command=lambda: controller.show_frame(RegisterPage))
+
         self.button1.grid(row=2, column=2, pady=(190, 5))
 
-        self.button1 = customtkinter.CTkButton(
+        self.button2 = customtkinter.CTkButton(
             self,
-            text="Sair",
-            command=self.destroy
+            text="Sair ",
+            command=self.controller.destroy  # aqui melhor usar o controller
         )
-        self.button1.grid(row=2, column=0, pady=(190, 5))
+        self.button2.grid(row=2, column=0, pady=(190, 5))
+
+
+class RegisterPage(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.usuarios = []
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+
+        self.entry_nome = customtkinter.CTkEntry(
+            self,
+            placeholder_text="     Nome do usuário",
+        )
+        self.entry_nome.grid(row=0, column=1, pady=(0, 140))
+
+        self.entry_born_date = customtkinter.CTkEntry(
+            self,
+            placeholder_text="  Data de nascimento",
+        )
+        self.entry_born_date.grid(row=0, column=1, pady=(0, 70))
+
+        self.entry_cpf = customtkinter.CTkEntry(
+            self,
+            placeholder_text="     CPF do usuário",
+        )
+        self.entry_cpf.grid(row=0, column=1, pady=(0, 0))
+
+        self.entry_address = customtkinter.CTkEntry(
+            self,
+            placeholder_text="     Qual seu estado?",
+        )
+        self.entry_address.grid(row=0, column=1, pady=(70, 0))
+
+        self.entry_password = customtkinter.CTkEntry(
+            self,
+            placeholder_text="     Senha do usuário",
+        )
+        self.entry_password.grid(row=0, column=1, pady=(140, 5))
+
+        self.button = customtkinter.CTkButton(
+            self,
+            text="Criar conta",
+            command=self.validate
+        )
+        self.button.grid(row=0, column=1, pady=(210, 5))
+
+        self.button = customtkinter.CTkButton(
+            self,
+            text="Voltar",
+            command=lambda: controller.show_frame(WalletLogin)
+        )
+        self.button.grid(row=3, column=0, pady=(0, 0))
+
+        self.button = customtkinter.CTkButton(
+            self,
+            text="REGRAS",
+            command=lambda: controller.show_frame(WalletLogin)
+        )
+        self.button.grid(row=3, column=2, pady=(0, 0))
+
+    def validate(self):
+        nome = self.entry_nome.get()
+        born_date = self.entry_born_date.get()
+        cpf = self.entry_cpf.get()
+        address = self.entry_address.get()
+        password = self.entry_password.get()
+
+        self.usuarios.append(
+            {"nome": nome, "born_date": born_date, "CPF": cpf,
+             "address": address, "senha": password, "saldo": 0, "extrato": ""}
+        )
+        self.entry_born_date.delete(0, 'end')
+        self.entry_nome.delete(0, 'end')
+        self.entry_cpf.delete(0, 'end')
+        self.entry_address.delete(0, 'end')
+        self.entry_password.delete(0, 'end')
+
+        self.controller.show_frame(WalletLogin)
+
+        print(self.usuarios)
+
+
+class HomePage(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_rowconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_rowconfigure(3, weight=1)
+
+        self.label = customtkinter.CTkLabel(self, text="Home Page")
+        self.label.grid(row=1, column=1, pady=(0, 0))
+
+        self.button = customtkinter.CTkButton(
+            self,
+            text="logout",
+            command=lambda: controller.show_frame(WalletLogin)
+        )
+        self.button.grid(row=2, column=0, pady=(190, 5))
 
 
 inicialização = WalletApp()
