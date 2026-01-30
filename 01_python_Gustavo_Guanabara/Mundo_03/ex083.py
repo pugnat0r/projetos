3bc7c218ac5fc5a473728320320206b6e89fbfe9
pugnator <pugnator_winchester@hotmail.com>
2026-01-29 21:59:05 -0300

    New: ex083.py New Solution

--- 01_python_Gustavo_Guanabara/Mundo_03/ex083.py
+++ 01_python_Gustavo_Guanabara/Mundo_03/ex083.py
@@ -1,7 +1,5 @@
 # Crie um programa onde o usuário digite uma expressão qualquer que use parênteses.
-# Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta. 
-
-
+# Seu aplicativo deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
 pagamentos_parenteses = 0
 
@@ -9,20 +7,16 @@ exp = str(input(f"Digite sua expressão: "))
 
 for i in exp:
 
-    if pagamentos_parenteses == 0 and i == ")":
-
-        print("Sua expressão está ERRADA!")
-        break
-
-    elif i == "(":
+    if i == "(":
         pagamentos_parenteses += 1
-    elif i  ==  ")":
+    elif i == ")":
         pagamentos_parenteses -= 1
+        if pagamentos_parenteses == -1:
+            break
+
 
 if pagamentos_parenteses == 0:
     print(f"A expressão está CERTA!")
+
 else:
     print(f"A expressão está ERRADA!")
-# testando dnv
-
-
