70e1e8efca07761684c37f60affbd97a477933a6
pugnator <pugnator_winchester@hotmail.com>
2026-02-02 23:40:40 -0300

    Modified: Ex087.py | Resolvendo

--- 01_python_Gustavo_Guanabara/Mundo_03/ex087.py
+++ 01_python_Gustavo_Guanabara/Mundo_03/ex087.py
@@ -3,31 +3,41 @@
 # B) A soma dos valores da terceira coluna.
 # C) O maior valor da segunda linha.
 
-matriz = [ [], [], [] ]
-PAR = 0
-Terceira_Coluna = 0
+
+from click import pause
+
+
+matriz = [[], [], []]
+Soma_Pares = 0
+Soma_Terceira_Coluna = 0
 Maior_Segunda_Linha = 0
 
 for coluna in range(0, 3):
-    for linha in range(0, 3):
+    for unidade in range(0, 3):
 
-        matriz[coluna].append(int(input(f"Digite um valor para {coluna}, {linha}: ")))
+        matriz[coluna].append(
+            int(input(f"Digite um valor para {coluna}, {unidade}: ")))
+
+print("-="*30)
+for pos, coluna in enumerate(matriz):
+    print(coluna)
+    pause()
+    print(f"{" ":<15}" +
+          f"[ {coluna[0]:^5}] [ {coluna[1]:^5} ] [ {coluna[2]:^5} ]")
+
+    for unidade in coluna:
+        if unidade % 2 == 0:
+            Soma_Pares += unidade
+
+        if pos == 1:
+            if unidade > Maior_Segunda_Linha:
+                Maior_Segunda_Linha = unidade
+
+    # Soma dos números da terceira coluna
+    Soma_Terceira_Coluna += coluna[2]
 
 print("-="*30)
 
-
-cont = 0
-for unidade in matriz:
-    print(" "*20+f"[ {unidade[0]} ]  [ {unidade[1]} ]  [ {unidade[2]} ]")
-
-    cont += 1
-
-    if unidade % 2 == 0:
-        PAR += unidade
-
-    Terceira_Coluna += unidade[2]
-    
-    if cont == 2:
-        if 
-    
-print("-="*30)
+print(f"A soma dos valores pares é: {Soma_Pares}")
+print(f"A soma dos valroes da terceira coluna é: {Soma_Terceira_Coluna}")
+print(f"O maior valor da segunda linha é: {Maior_Segunda_Linha}")
