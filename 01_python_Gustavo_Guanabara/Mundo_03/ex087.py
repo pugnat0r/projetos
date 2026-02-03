e77c075f2226ba1dfbf2f7c2257fea00f4a0348b
pugnat0r <pugnator_winchester@hotmail.com>
2026-02-03 07:46:47 -0300

    Resolvido ex087.py

--- 01_python_Gustavo_Guanabara/Mundo_03/ex087.py
+++ 01_python_Gustavo_Guanabara/Mundo_03/ex087.py
@@ -4,28 +4,24 @@
 # C) O maior valor da segunda linha.
 
 
-from click import pause
-
-
 matriz = [[], [], []]
 Soma_Pares = 0
 Soma_Terceira_Coluna = 0
 Maior_Segunda_Linha = 0
 
-for coluna in range(0, 3):
+for linha in range(0, 3):
     for unidade in range(0, 3):
 
-        matriz[coluna].append(
-            int(input(f"Digite um valor para {coluna}, {unidade}: ")))
+        matriz[linha].append(
+            int(input(f"Digite um valor para {linha}, {unidade}: ")))
 
 print("-="*30)
-for pos, coluna in enumerate(matriz):
-    print(coluna)
-    pause()
-    print(f"{" ":<15}" +
-          f"[ {coluna[0]:^5}] [ {coluna[1]:^5} ] [ {coluna[2]:^5} ]")
+for pos, linha in enumerate(matriz):
+   # print(f'A linha {pos} contém {linha}')
+  
+    print(f'{" ":<15}[ {linha[0]:^5}] [ {linha[1]:^5} ] [ {linha[2]:^5} ]')
 
-    for unidade in coluna:
+    for unidade in linha:
         if unidade % 2 == 0:
             Soma_Pares += unidade
 
@@ -34,7 +30,7 @@ for pos, coluna in enumerate(matriz):
                 Maior_Segunda_Linha = unidade
 
     # Soma dos números da terceira coluna
-    Soma_Terceira_Coluna += coluna[2]
+    Soma_Terceira_Coluna += linha[2]
 
 print("-="*30)
 
