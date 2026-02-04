426c8b2196d21c7368d7f866f974dcb64c36ee4c
pugnator <pugnator_winchester@hotmail.com>
2026-02-04 20:40:52 -0300

    Resolvido ex088.py

--- 01_python_Gustavo_Guanabara/Mundo_03/ex088.py
+++ 01_python_Gustavo_Guanabara/Mundo_03/ex088.py
@@ -1,23 +1,40 @@
 from random import randint
-
-from click import clear
+import time
 
 palpites = []
 number = list()
 
 while True:
-    qtd = int(input("Quantos jogos deseja?: "))
+    qtd = int(input("Quantos jogos você quer que eu sorteie?: "))
 
     for i in range(0, qtd):
 
         for n in range(0, 6):
-            number.append(randint(0, 60))
+            while True:
 
+                n_temp = randint(1, 60)
+
+                if n_temp not in number:
+                    number.append(n_temp)
+                    break
+
+        number.sort()
         palpites.append(number[:])
         number.clear()
 
     if i == qtd - 1:
         break
 
+print()
+print("-"*35)
+print(f"{'JOGO NA MEGA SENA':^35}")
+print("-"*35)
+
+print()
 for pos, item in enumerate(palpites):
-    print(f"Seu {pos+1}° palpite: {item}")
+    print(f"Jogo {pos+1}: {item}")
+    time.sleep(1)
+print()
+
+print(f"{"< BOA SORTE >":-^35}")
+print()
